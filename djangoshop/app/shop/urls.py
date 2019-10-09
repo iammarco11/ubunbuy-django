8b299452058f8bc8c2c191c8ed95206050344854
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2019-10-09 21:59:30 +0530

    found a way to display the search through views.py

--- djangoshop/app/shop/urls.py
+++ djangoshop/app/shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('<str:searchtext>/search/', views.result, name='result'),
 ]
 
--- djangoshop/app/shop/views.py
+++ djangoshop/app/shop/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from .models import search
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse("Hello, world. You're at the polls index.")
+def result(request,searchtext):
+	response = "You searched for %s"
+	return HttpResponse(response % searchtext)	
