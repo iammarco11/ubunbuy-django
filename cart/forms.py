3ee572009cc244504f3853d3876faead91da948d
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2019-10-29 20:36:00 +0530

    Fixed the bug of adding negative number and decimal number of products

--- cart/forms.py
+++ cart/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from .models import Cart ,Login, Product
 
+
 class CartForm(forms.ModelForm):
-    
+    quantity = forms.IntegerField(min_value=1)
     class Meta:
         model = Cart
         fields = ['product','quantity']
         widgets = {
             'product':forms.HiddenInput(),
+            
         }
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput())
